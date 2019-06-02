75ed5bc20861fa66f895bd89030d5684925cc965
Shankar Govindarajan <shgovind@redhat.com>
2019-06-02 13:24:14 -0500

    Changed to spring boot 2.0

--- src/main/java/com/innov/demo/CashBackApplication.java
+++ src/main/java/com/innov/demo/CashBackApplication.java
@@ -2,27 +2,9 @@ package com.innov.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.logging.Logger;
-
-import com.uber.jaeger.Configuration;
-import com.uber.jaeger.samplers.ConstSampler;    
-
-import io.opentracing.Tracer;
 
 @SpringBootApplication
 public class CashBackApplication {
-
-	private static final Logger logger = Logger.getLogger(CashBackApplication.class.getName());
-	
-	@Bean
-	public Tracer jaegerTracer() {
-		return new Configuration("cash-back", new Configuration.SamplerConfiguration(ConstSampler.TYPE, 1),
-				new Configuration.ReporterConfiguration())
-				.getTracer();
-	}
-
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CashBackApplication.class, args);
